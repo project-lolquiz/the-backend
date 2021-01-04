9e12719e492f55e06da33afff41253a59191e4f6
felipe-basina <felipe.basina@gmail.com>
2021-01-04 20:33:17 -0300

    Adicionado rotas iniciais da aplicacao.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/ping')
+def ping():
+    print("I'am online!!!")
+    return "pong"
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    print(e)
+    return "<strong>It seems this is not correct :-(</strong>", 404
+
+
+if __name__ == '__main__':
+    app.run()
