d1329e43bf072d40b76514a1b652ee6dd9d3019d
felipe-basina <felipe.basina@gmail.com>
2021-01-07 20:56:24 -0300

    Atualizado script para iniciar o ambiente local.

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
+############################################################
+######### Run this script with . ./run.sh command ##########
+############################################################
+
 # Running docker-compose
 sudo docker-compose down
 sudo docker-compose up -d --remove-orphans
 
 # Setting environment variables
 export REDIS_PASSWORD=Acesso01!
+export ENV=dev
+export DATABASE_URL=postgresql://postgres@localhost:5432/lolquiz
 
 echo 'Waiting containers to be up running...'
 sleep 10s
@@ -12,4 +18,8 @@ sleep 10s
 echo 'Running migrations'
 cat ./migrations/*.sql | docker exec -i the-backend_thedb_1 psql -U postgres -d lolquiz
 
-echo 'Infrastructure OK!'
+echo 'Infrastructure OK'
+
+source venv/bin/activate
+pip install -r requirements.txt
+python main.py
