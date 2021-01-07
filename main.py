5303c0feeea164d13d065d46ac7ffe653123a50e
felipe-basina <felipe.basina@gmail.com>
2021-01-07 20:57:24 -0300

    Definido novo arquivo inicial (entrypoint) do projeto.

---
+++ main.py
@@ -0,0 +1,19 @@
+from app import create_app
+from routes.default_route import default_rest
+from routes.dumb_route import dumb_rest
+
+default_prefix = '/lolquiz'
+
+app = create_app()
+app.register_blueprint(default_rest)
+app.register_blueprint(dumb_rest, url_prefix=default_prefix)
+
+
+@app.errorhandler(404)
+def http_not_found(e):
+    print(e)
+    return "<strong>It seems this is not correct :-(</strong>", 404
+
+
+if __name__ == '__main__':
+    app.run()
