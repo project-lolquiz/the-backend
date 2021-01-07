3b0a4bc64dbe1740af131d9a11791e305fadb56d
felipe-basina <felipe.basina@gmail.com>
2021-01-06 22:39:32 -0300

    Adicionado comandos para execucao de migracoes apos o container da base de dados ser ativado.

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 # Running docker-compose
-docker-compose down
-docker-compose up -d
+sudo docker-compose down
+sudo docker-compose up -d --remove-orphans
 
 # Setting environment variables
 export REDIS_PASSWORD=Acesso01!
+
+echo 'Waiting containers to be up running...'
+sleep 10s
+
+# Running scripts into database
+echo 'Running migrations'
+cat ./migrations/*.sql | docker exec -i the-backend_thedb_1 psql -U postgres -d lolquiz
+
+echo 'Infrastructure OK!'
