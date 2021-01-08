afffe63dc5f45e521c5f8c2f0afdb3b4307187b1
felipe-basina <felipe.basina@gmail.com>
2021-01-07 21:23:03 -0300

    Adicionado dependencia para conexao com redis.

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from configs.db_config import get_config
+from configs.db_config import get_config as db_config
+from configs.redis_config import RedisConfig
 
 db = SQLAlchemy()
+_redis = RedisConfig()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(get_config())
+    app.config.from_object(db_config())
     db.init_app(app)
     return app
 
 
 def get_db_connection():
     return db
+
+
+def get_redis_connection():
+    return _redis.get_connection()
