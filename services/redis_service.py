46c751ccfb2e6f441fbfda0b71f94e6c53d71c40
felipe-basina <felipe.basina@gmail.com>
2021-01-08 18:32:00 -0300

    Adicionado operacoes para criar e recuperar por chave valores no redis.

---
+++ services/redis_service.py
@@ -0,0 +1,22 @@
+from app import get_redis_connection
+
+_redis = get_redis_connection()
+
+
+def add(content):
+    expires_in = content['expires_in']
+    if expires_in is None:
+        expires_in = 10
+    _redis.set(content['key'], content['value'], expires_in)
+
+
+def get_by_key(key):
+    return _redis.get(key)
+
+
+def get_all():
+    _redis.set('full stack', 'python')
+    _redis.set('version', '3')
+    _redis.set('framework', 'flask')
+    keys = _redis.keys()
+    return {keys[index]: _redis.get(keys[index]) for index in range(0, len(keys))}
