277cf498e1491d232b40297b7c0f22088f9664ae
felipe-basina <felipe.basina@gmail.com>
2021-01-08 22:01:27 -0300

    Adicionado operacoes para remocao de registros no redis.

--- services/redis_service.py
+++ services/redis_service.py
@@ -1,3 +1,5 @@
+import json
+
 from app import get_redis_connection
 
 _redis = get_redis_connection()
@@ -7,7 +9,8 @@ def add(content):
     expires_in = content['expires_in']
     if expires_in is None:
         expires_in = 10
-    _redis.set(content['key'], content['value'], expires_in)
+    _json = json.dumps(content)
+    _redis.set(content['key'], _json, expires_in)
 
 
 def get_by_key(key):
@@ -20,3 +23,12 @@ def get_all():
     _redis.set('framework', 'flask')
     keys = _redis.keys()
     return {keys[index]: _redis.get(keys[index]) for index in range(0, len(keys))}
+
+
+def delete_all():
+    for key in _redis.keys():
+        delete_by_key(key)
+
+
+def delete_by_key(key):
+    _redis.delete(key)
