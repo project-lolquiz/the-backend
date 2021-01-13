00ef19a7f632aa07382ba00452f202e300ba1ab3
felipe-basina <felipe.basina@gmail.com>
2021-01-13 17:20:34 -0300

    Adicionado mapeamento da rota para health check.

--- routes/default_route.py
+++ routes/default_route.py
@@ -4,7 +4,7 @@ default_rest = Blueprint('default_rest', __name__)
 
 
 @default_rest.route('/')
+@default_rest.route('/health')
 @default_rest.route('/ping')
 def ping():
-    print("I'am online!!!")
     return "pong", 200
