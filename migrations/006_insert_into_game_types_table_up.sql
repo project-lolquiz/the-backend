d4bdc58e3f2ced43b4cc8253b57555e607d9f920
felipe-basina <felipe.basina@gmail.com>
2021-01-13 19:53:16 -0300

    Removido acentuacoes.

--- migrations/006_insert_into_game_types_table_up.sql
+++ migrations/006_insert_into_game_types_table_up.sql
@@ -1,3 +1,3 @@
-INSERT INTO game_types (name, description, created_at, updated_at) values ('o-que-voce-prefere', 'O quê você prefere?', now(), now());
-INSERT INTO game_types (name, description, created_at, updated_at) values ('o-mais-provavel', 'O mais provável', now(), now());
+INSERT INTO game_types (name, description, created_at, updated_at) values ('o-que-voce-prefere', 'O que voce prefere?', now(), now());
+INSERT INTO game_types (name, description, created_at, updated_at) values ('o-mais-provavel', 'O mais provavel', now(), now());
 INSERT INTO game_types (name, description, created_at, updated_at) values ('mistura-louca', 'Mistura louca', now(), now());
