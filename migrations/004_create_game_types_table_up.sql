c2231af7e81d5e5a288f914c5c8744fb7537877d
felipe-basina <felipe.basina@gmail.com>
2021-01-13 18:36:45 -0300

    Adicionado colunas de datas em migracoes game_types e game_modes.

--- migrations/004_create_game_types_table_up.sql
+++ migrations/004_create_game_types_table_up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE "game_types" (
   "id" SERIAL PRIMARY KEY,
   "name" varchar NOT NULL,
-  "description" varchar
+  "description" varchar,
+  "created_at" timestamp NOT NULL,
+  "updated_at" timestamp NOT NULL default current_timestamp
 );
