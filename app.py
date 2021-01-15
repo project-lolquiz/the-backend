05863ea4e6c5e1d4de05894e30c90885f5f0acfa
felipe-basina <felipe.basina@gmail.com>
2021-01-14 21:23:15 -0300

    Definido template para swagger.

--- app.py
+++ app.py
@@ -7,13 +7,28 @@ from configs.redis_config import get_connection as redis_connection
 
 db = SQLAlchemy()
 _redis = redis_connection()
+swagger_template = {
+          "swagger": "2.0",
+          "info": {
+            "title": "Lolquiz API",
+            "description": "API for Lolquiz",
+            "contact": {
+              "responsibleOrganization": "",
+              "responsibleDeveloper": "",
+              "email": "",
+              "url": "",
+            },
+            "termsOfService": "tof",
+            "version": "0.0.1"
+          }
+        }
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(db_connection())
     db.init_app(app)
-    Swagger(app)
+    Swagger(app, template=swagger_template)
     return app
 
 
