a711674a7ae1dbbec4463e84e44298dc8a0a0a84
felipe-basina <felipe.basina@gmail.com>
2021-01-25 22:10:10 -0300

    Adicionado teste de unidade para rota exemplo que adiciona dados em redis.

--- tests/routes/test_dumb.py
+++ tests/routes/test_dumb.py
@@ -6,6 +6,8 @@ import pytest
 import main
 from main import default_prefix
 
+APPLICATION_JSON = "application/json"
+
 
 @pytest.fixture
 def client():
@@ -21,9 +23,27 @@ def test_redis_get_by_key(mock_redis_service, client):
 
     response = client.get(default_prefix + '/redis-get-by-key/redis-key')
     assert response.data
-    assert response.content_type == 'application/json'
+    assert response.content_type == APPLICATION_JSON
     assert response.status_code == 200
 
     response_content = json.loads(response.get_data(as_text=True))
     assert response_content['key'] == 'redis-key'
     assert response_content['value'] == 'redis-value'
+
+
+@mock.patch('routes.dumb_route.into_redis')
+def test_redis_add(mock_redis_service, client):
+    mock_redis_service.return_value = None
+
+    json_body = {'key': 'the-key', 'value': 'the-value', 'expires_in': 10}
+    response = client.post(default_prefix + '/redis-add',
+                           data=json.dumps(json_body),
+                           content_type=APPLICATION_JSON)
+    assert response.data
+    assert response.content_type == APPLICATION_JSON
+    assert response.status_code == 201
+
+    response_content = json.loads(response.get_data(as_text=True))
+    assert response_content['key'] == 'the-key'
+    assert response_content['value'] == 'the-value'
+    assert response_content['expires_in'] == 10
