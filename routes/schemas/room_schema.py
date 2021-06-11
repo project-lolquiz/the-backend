4ddf502dcdfcbeb4c20e48458e9a165078312972
felipe-basina <felipe.basina@gmail.com>
2021-06-10 22:23:50 -0300

    Adicionado schema para validacao de contrato ao criar uma nova sala de jogo.

---
+++ routes/schemas/room_schema.py
@@ -0,0 +1,17 @@
+post_schema = {
+    'type': 'object',
+    'properties': {
+        'game_type': {'type': 'integer'},
+        'game_mode': {'type': 'integer'},
+        'host_user': {
+            'type': 'object',
+            'properties': {
+                'uid': {'type': 'string'},
+                'nickname': {'type': 'string'}
+            },
+            'required': ['uid', 'nickname']
+        },
+        'total_rounds': {'type': 'integer'},
+    },
+    'required': ['game_type', 'game_mode', 'total_rounds']
+}
