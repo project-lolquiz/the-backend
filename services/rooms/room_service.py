c5cf47c0c6e344a4537d09744a3f9c6d01774ee3
felipe-basina <felipe.basina@gmail.com>
2021-06-10 21:48:26 -0300

    Adicionado operacao para verificar existencia de sala.

--- services/rooms/room_service.py
+++ services/rooms/room_service.py
@@ -2,7 +2,7 @@ import json
 import random
 import string
 
-from services.redis_service import set_content
+from services.redis_service import set_content, get_by_key
 
 
 def create_room(content):
@@ -11,6 +11,10 @@ def create_room(content):
     room_data['room_id'] = room_id
     set_content(room_id, room_data)
     return room_id
+
+
+def exists_room_by_id(room_id):
+    return get_by_key(room_id) is not None
 
 
 def generate_room_id():
