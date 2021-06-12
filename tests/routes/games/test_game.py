e9d9ffb2e81b14f08f8577dc430294ae4af89013
felipe-basina <felipe.basina@gmail.com>
2021-06-12 11:41:00 -0300

    Adicionado teste de unidade de rota para criacao de sala de jogo com codigo invalido.

--- tests/routes/games/test_game.py
+++ tests/routes/games/test_game.py
@@ -36,3 +36,19 @@ def test_success_create_game_room(client):
     assert response
     assert response.content_type == TEXT_HTML_UTF8
     assert response.status_code == 201
+
+
+def test_failure_create_game_room_with_room_not_found(client):
+    room_id = 'DEFG'
+    response = client.post(default_prefix + '/games/{}/start'.format(room_id),
+                           data=json.dumps(request_start_game_body()),
+                           content_type=APPLICATION_JSON)
+
+    assert response
+    assert response.content_type == APPLICATION_JSON
+    assert response.status_code == 404
+
+    response_content = json.loads(response.get_data(as_text=True))
+    assert 'error' in response_content
+    assert 'timestamp' in response_content
+    assert response_content['error'] == 'Room ID {} not found'.format(room_id)
