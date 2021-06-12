82798cc50108c4a20059ec41dcac8b839d13dd06
felipe-basina <felipe.basina@gmail.com>
2021-06-12 11:32:47 -0300

    Correcao nomenclatura de diretorio para sala de jogo.

--- routes/rooms/room_route.py
+++ routes/rooms/room_route.py
@@ -10,14 +10,14 @@ room_rest = Blueprint('room_rest', __name__)
 
 @room_rest.route('/games/rooms', methods=['post'])
 @expects_json(post_schema)
-@swag_from('../docs/room/room_create.yml')
+@swag_from('../docs/rooms/room_create.yml')
 def create_game_room():
     response = {'room_id': create_room(request.get_json())}
     return jsonify(response), 201
 
 
 @room_rest.route('/games/rooms/<string:room_id>')
-@swag_from('../docs/room/room_get.yml')
+@swag_from('../docs/rooms/room_get.yml')
 def check_exists_room(room_id):
     response = {'exists': exists_room_by_id(room_id)}
     return jsonify(response), 200
