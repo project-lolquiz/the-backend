812bc159ff117657a0f51d74e72e4c4b9106c7cf
felipe-basina <felipe.basina@gmail.com>
2021-06-12 12:03:36 -0300

    Adicionado cenarios de testes de unidade para validar atributos obrigatorios na criacao de sala de jogo.

--- tests/routes/games/test_game.py
+++ tests/routes/games/test_game.py
@@ -52,3 +52,61 @@ def test_failure_create_game_room_with_room_not_found(client):
     assert 'error' in response_content
     assert 'timestamp' in response_content
     assert response_content['error'] == 'Room ID {} not found'.format(room_id)
+
+
+def test_failure_create_game_room_with_required_missing_properties(client):
+    game_room_response = create_game_room(client, request_room_body())
+
+    response_content = json.loads(game_room_response.get_data(as_text=True))
+    room_id = response_content['room_id']
+
+    key_to_remove = 'users'
+    json_body = request_start_game_body()
+    del json_body[key_to_remove]
+
+    response = client.post(default_prefix + '/games/{}/start'.format(room_id),
+                           data=json.dumps(json_body),
+                           content_type=APPLICATION_JSON)
+
+    assert_failure_missing_property(response, key_to_remove)
+
+
+def with_users_empty(client):
+    game_room_response = create_game_room(client, request_room_body())
+
+    response_content = json.loads(game_room_response.get_data(as_text=True))
+    room_id = response_content['room_id']
+
+    json_body = {'users': [{}]}
+
+    response = client.post(default_prefix + '/games/{}/start'.format(room_id),
+                           data=json.dumps(json_body),
+                           content_type=APPLICATION_JSON)
+
+    assert_failure_missing_property(response, 'uid')
+
+
+def without_nickname(client):
+    game_room_response = create_game_room(client, request_room_body())
+
+    response_content = json.loads(game_room_response.get_data(as_text=True))
+    room_id = response_content['room_id']
+
+    json_body = {'users': [{'uid': '8852c5af-a6e5-4fad-9022-3d10ed111a30'}]}
+
+    response = client.post(default_prefix + '/games/{}/start'.format(room_id),
+                           data=json.dumps(json_body),
+                           content_type=APPLICATION_JSON)
+
+    assert_failure_missing_property(response, 'nickname')
+
+
+def assert_failure_missing_property(response, missing_property):
+    assert response.data
+    assert response.content_type == APPLICATION_JSON
+    assert response.status_code == 400
+
+    response_content = json.loads(response.get_data(as_text=True))
+    assert 'error' in response_content
+    assert 'timestamp' not in response_content
+    assert response_content['error'] == '\'{}\' is a required property'.format(missing_property)
