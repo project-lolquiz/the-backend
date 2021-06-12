9827ca4e3df32ff8c67ece0b89268f01ae791a42
felipe-basina <felipe.basina@gmail.com>
2021-06-11 22:10:31 -0300

    Alteracao do nome da rota, adicionado o termo config.

--- routes/game_route.py
+++ routes/game_route.py
@@ -3,16 +3,16 @@ from flask import jsonify, Blueprint
 
 from services.game_service import *
 
-game_rest = Blueprint('game_rest', __name__)
+game_config_rest = Blueprint('game_config_rest', __name__)
 
 
-@game_rest.route('/games/types')
+@game_config_rest.route('/games/configs/types')
 @swag_from('./docs/game/game_types.yml')
 def game_types():
     return jsonify(get_all_types()), 200
 
 
-@game_rest.route('/games/modes')
+@game_config_rest.route('/games/configs/modes')
 @swag_from('./docs/game/game_modes.yml')
 def game_modes():
     return jsonify(get_all_modes()), 200
