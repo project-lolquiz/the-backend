ac9ad1517b7db3ced3b5a178036c6a612fafd259
felipe-basina <felipe.basina@gmail.com>
2021-06-13 20:26:25 -0300

    Adicionado dicionario com infos para configuracao do swagger e assim definir o contexto base para exibicao da ui.

--- app.py
+++ app.py
@@ -7,28 +7,46 @@ from configs.redis_config import get_connection as redis_connection
 
 db = SQLAlchemy()
 _redis = redis_connection()
+
 swagger_template = {
-          "swagger": "2.0",
-          "info": {
-            "title": "Lolquiz API",
-            "description": "API for Lolquiz",
-            "contact": {
-              "responsibleOrganization": "",
-              "responsibleDeveloper": "",
-              "email": "",
-              "url": "",
-            },
-            "termsOfService": "tof",
-            "version": "0.0.1"
-          }
+    "swagger": "2.0",
+    "info": {
+        "title": "Lolquiz API",
+        "description": "API for Lolquiz",
+        "contact": {
+            "responsibleOrganization": "",
+            "responsibleDeveloper": "",
+            "email": "",
+            "url": "",
+        },
+        "termsOfService": "tof",
+        "version": "0.0.1"
+    }
+}
+
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
         }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
 
 
-def create_app():
+def create_app(default_prefix):
     app = Flask(__name__)
     app.config.from_object(db_connection())
     db.init_app(app)
-    Swagger(app, template=swagger_template)
+    swagger_config['url_prefix'] = default_prefix
+    Swagger(app, template=swagger_template, config=swagger_config)
     return app
 
 
