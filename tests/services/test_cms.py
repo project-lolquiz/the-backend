a8d99f9a7b58aa6fb752a5e1b357f6cdc8afa7c1
felipe-basina <felipe.basina@gmail.com>
2021-09-04 01:29:41 -0300

    Adicionado testes de unidade para cenario de recuperacao de perguntas de acordo com o tipo e modo de jogo.

--- tests/services/test_cms.py
+++ tests/services/test_cms.py
@@ -1,12 +1,14 @@
 import os
 
 from unittest import mock
+
 import pytest
 
 os.environ['ENV'] = 'qa'
 
 from components.exception_component import QuestionsNotFound
-from services.cms_service import ALL_QUESTIONS_KEY, save_questions_on_redis, get_all_questions
+from services.cms_service import ALL_QUESTIONS_KEY, save_questions_on_redis, get_all_questions, \
+    get_questions_by_game_type_and_game_mode
 from services.redis_service import get_by_key
 
 
@@ -49,3 +51,27 @@ def test_failure_get_all_questions_with_questions_not_found(mock_get_by_key, moc
 
     with pytest.raises(QuestionsNotFound, match='No questions were found'):
         get_all_questions()
+
+
+def test_get_questions_by_game_type_and_game_mode():
+    game_type_id = 2
+    game_mode_id = 1
+
+    questions = get_questions_by_game_type_and_game_mode(game_type_id, game_mode_id)
+    assert questions is not None
+    for question in questions:
+        assert 'id' in question
+        assert 'title' in question
+        assert 'options' in question
+
+
+@mock.patch('services.cms_service.get_questions')
+@mock.patch('services.cms_service.get_by_key')
+def test_failure_get_questions_by_game_type_and_game_mode_with_questions_not_found(mock_get_by_key, mock_get_questions):
+    mock_get_by_key.return_value = None
+    mock_get_questions.return_value = None
+
+    with pytest.raises(QuestionsNotFound, match='No questions were found'):
+        game_type_id = 2
+        game_mode_id = 1
+        get_questions_by_game_type_and_game_mode(game_type_id, game_mode_id)
