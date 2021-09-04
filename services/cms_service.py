5f68f15dc2b7db14e68c9559893c675c6564686b
felipe-basina <felipe.basina@gmail.com>
2021-09-04 01:28:24 -0300

    Adicionado operacao para recuperar perguntas de acordo com o tipo e mode de jogo enviados como parametros.

--- services/cms_service.py
+++ services/cms_service.py
@@ -22,3 +22,22 @@ def get_all_questions():
         questions = get_by_key(ALL_QUESTIONS_KEY)
 
     return json.loads(questions)
+
+
+def get_questions_by_game_type_and_game_mode(game_type_id, game_mode_id):
+    questions = get_all_questions()
+
+    filtered_by_game_type = [question for question in questions if question['game_type']['id'] == game_type_id]
+    filtered_by_game_mode = [question for question in filtered_by_game_type
+                             if (same_game_mode_id(game_mode_id, question['game_modes']))]
+
+    filtered_questions = [{'id': question['id'],
+                           'title': question['title'],
+                           'options': question['options']} for question in filtered_by_game_mode]
+
+    return filtered_questions
+
+
+def same_game_mode_id(game_mode_id, game_modes):
+    ids = [game_mode['id'] for game_mode in game_modes]
+    return game_mode_id in ids
