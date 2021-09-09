aff61c4a560fa825b8a2a45e1c5413b0d827a7a7
felipe-basina <felipe.basina@gmail.com>
2021-09-08 22:41:01 -0300

    Refatoracao da funcao para recuperar info sobre rodada atraves do objeto recuperado do cache.

--- services/games/questions/answers/answer_service.py
+++ services/games/questions/answers/answer_service.py
@@ -66,7 +66,7 @@ def set_answer(room_id, content):
     current_game_status = set_current_game_status_for_selected_user(selected_user_id, current_game_status)
 
     draw_game = False
-    end_game = is_end_game(room_id, current_room)
+    end_game = is_end_game(current_room)
     if end_game:
         draw_game = is_draw_game(current_scores)
 
@@ -127,6 +127,6 @@ def set_current_game_status_for_selected_user(selected_user_id, current_game_sta
     return current_game_status
 
 
-def is_end_game(room_id, current_room):
-    current_round = get_game_round(room_id, current_room)
+def is_end_game(current_room):
+    current_round = current_room['game']['round']
     return current_round['current'] == current_round['total']
