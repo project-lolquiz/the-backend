48b0ccb0b908db68edf627d28948dd1d7c175cb3
felipe-basina <felipe.basina@gmail.com>
2021-09-24 08:13:43 -0300

    Adicionado testes de unidade para operacao de alterar o usuario host do jogo.

--- tests/services/rooms/test_room.py
+++ tests/services/rooms/test_room.py
@@ -1,8 +1,15 @@
+import json
 import os
+import pytest
+import uuid
+
+from components.exception_component import RoomNotFound
 
 os.environ['ENV'] = 'qa'
 
-from services.rooms.room_service import create_room, exists_room_by_id, generate_room_id
+from services.games.game_service import start_new_game
+from services.redis_service import set_content, get_by_key
+from services.rooms.room_service import create_room, exists_room_by_id, generate_room_id, change_host_user
 
 DEFAULT_TOTAL_ROUNDS = 10
 DEFAULT_SELECTED_USER_UID = '4b8c2cfe-e0f1-4e8b-b289-97f4591e2069'
@@ -33,6 +40,35 @@ def test_success_generate_room_id():
     assert len(room_id) == 4
 
 
+def test_success_change_host_user():
+    room_body = request_room_body()
+    room_id = create_room(room_body)
+    game_start_body = request_start_game_body()
+    current_room = json.loads(start_new_game(game_start_body, room_id))
+    current_room['game']['selected_users'] = []
+    set_content(room_id, current_room)
+
+    new_host_user = request_host_user_body(game_start_body['users'][0])
+    current_room = change_host_user(room_id, new_host_user)
+
+    changed_host_user = current_room['host_user']
+    assert changed_host_user
+    assert changed_host_user['uid'] == new_host_user['uid']
+    assert changed_host_user['nickname'] == new_host_user['nickname']
+
+    current_users = current_room['game']['users']
+    assert len(current_users) == 1
+    assert len([user for user in current_users
+                if user['uid'] == changed_host_user['uid']]) == 0
+
+
+def test_failure_change_host_user_with_room_not_found():
+    room_id = '2FV5'
+
+    with pytest.raises(RoomNotFound, match='Room ID {} not found'.format(room_id)):
+        change_host_user(room_id, None)
+
+
 def request_room_body():
     return {'game_type': 2,
             'game_mode': 1,
@@ -41,3 +77,15 @@ def request_room_body():
                 'nickname': 'john-doe'
             },
             'total_rounds': DEFAULT_TOTAL_ROUNDS}
+
+
+def request_start_game_body():
+    return {'users': [{'uid': str(uuid.uuid4()), 'nickname': 'bet64'},
+                      {'uid': str(uuid.uuid4()), 'nickname': 'avaii'}]}
+
+
+def request_host_user_body(new_host_user):
+    return {
+        'uid': new_host_user['uid'],
+        'nickname': new_host_user['nickname']
+    }
