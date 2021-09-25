fb6bf1d08c613fc0a7367c517dafe33f99f65a76
felipe-basina <felipe.basina@gmail.com>
2021-09-24 22:31:02 -0300

    Adicionado testes de aceitacao para operacao de atualizacao de usuario host.

---
+++ tests/features/game_host_user.feature
@@ -0,0 +1,43 @@
+Feature: Changing a host user
+
+  Scenario: Change a host user with empty selected users list
+    Given I request to create a new room with
+      | game_type | game_mode | host_user_uid                        | host_user_nickname | total_rounds |
+      | 2         | 1         | 48f51f68-9337-4d91-abdb-4bd5feebf1ac | Handsome Guy       | 3            |
+    And I request to start a new game with users
+      | uid                                  | nickname |
+      | a362d801-908a-4d67-b82c-078068269654 | User 1   |
+      | d3dce936-66a6-4653-ad36-335110249c86 | User 2   |
+    And there is a selected users list
+    When I request to set the new host user as
+      | uid                                  | nickname |
+      | a362d801-908a-4d67-b82c-078068269654 | User 1   |
+    Then I should get a 204 http response code
+    And there will be only 2 users in the game
+    And there will be only 0 users in the selected users list
+    And the host user is not
+      | uid                                  | nickname     |
+      | 48f51f68-9337-4d91-abdb-4bd5feebf1ac | Handsome Guy |
+
+  Scenario: Change a host user with a valid selected users list
+    Given I request to create a new room with
+      | game_type | game_mode | host_user_uid                        | host_user_nickname | total_rounds |
+      | 2         | 1         | 48f51f68-9337-4d91-abdb-4bd5feebf1ac | Handsome Guy       | 3            |
+    And I request to start a new game with users
+      | uid                                  | nickname |
+      | a362d801-908a-4d67-b82c-078068269654 | User 1   |
+      | d3dce936-66a6-4653-ad36-335110249c86 | User 2   |
+    And there is a selected users list with
+      | uid                                  |
+      | 48f51f68-9337-4d91-abdb-4bd5feebf1ac |
+      | a362d801-908a-4d67-b82c-078068269654 |
+      | d3dce936-66a6-4653-ad36-335110249c86 |
+    When I request to set the new host user as
+      | uid                                  | nickname |
+      | a362d801-908a-4d67-b82c-078068269654 | User 1   |
+    Then I should get a 204 http response code
+    And there will be only 2 users in the game
+    And there will be only 2 users in the selected users list
+    And the host user is not
+      | uid                                  | nickname     |
+      | 48f51f68-9337-4d91-abdb-4bd5feebf1ac | Handsome Guy |
---
+++ tests/features/steps/game_host_user.py
@@ -0,0 +1,75 @@
+import json
+
+from behave import *
+from main import default_prefix
+from services.redis_service import get_by_key, set_content
+
+APPLICATION_JSON = 'application/json'
+
+use_step_matcher("re")
+
+
+@step("there is a selected users list")
+def there_is_a_selected_users_list(context):
+    current_room = json.loads(get_by_key(context.world.room_id))
+    current_room['game']['selected_users'] = []
+    set_content(context.world.room_id, current_room)
+
+
+@step("there is a selected users list with")
+def there_is_a_selected_users_list_with(context):
+    selected_users = []
+    for row in context.table:
+        selected_users.append(row['uid'])
+
+    current_room = json.loads(get_by_key(context.world.room_id))
+    current_room['game']['selected_users'] = selected_users
+    set_content(context.world.room_id, current_room)
+
+
+@when("I request to set the new host user as")
+def i_request_to_set_the_new_host_user_as(context):
+    for row in context.table:
+        context.world.request_body = {'uid': row['uid'],
+                                      'nickname': row['nickname']}
+    context.world.response_body = set_host_user(context)
+
+
+@step("there will be only (\d+) users in the game")
+def there_will_be_only_users_in_the_game(context, total_users):
+    current_room = json.loads(get_by_key(context.world.room_id))
+    current_game = current_room['game']
+    users = current_game['users']
+    host_user = current_room['host_user']
+    users.append(host_user)
+
+    assert len(users) == int(total_users)
+
+
+@step("there will be only (\d+) users in the selected users list")
+def there_will_be_only_users_in_the_game(context, total_users):
+    current_room = json.loads(get_by_key(context.world.room_id))
+    current_game = current_room['game']
+    selected_users = current_game['selected_users']
+
+    assert len(selected_users) == int(total_users)
+
+
+@step("the host user is not")
+def the_host_user_is_not(context):
+    previous_host_user = {}
+    for row in context.table:
+        previous_host_user = {'uid': row['uid'],
+                              'nickname': row['nickname']}
+
+    current_room = json.loads(get_by_key(context.world.room_id))
+    host_user = current_room['host_user']
+
+    assert previous_host_user['uid'] != host_user['uid']
+    assert previous_host_user['nickname'] != host_user['nickname']
+
+
+def set_host_user(context):
+    return context.world.client.put(default_prefix + '/games/rooms/{}/host_user'.format(context.world.room_id),
+                                    data=json.dumps(context.world.request_body),
+                                    content_type=APPLICATION_JSON)
