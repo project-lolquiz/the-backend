46ff8e3f5c6b61ef2bb9225bea0c5ddff52584b7
felipe-basina <felipe.basina@gmail.com>
2021-10-23 14:00:36 -0300

    Refatoracao para realizar busca paginada; adicionado validacao para garantir mapeamento somente de perguntas validas.

--- gateways/cms_gateway.py
+++ gateways/cms_gateway.py
@@ -4,8 +4,10 @@ import json
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-LOLQUIZ_CMS_URL = 'https://lolquiz-cms.herokuapp.com/questions?_sort=id'
+LOLQUIZ_CMS_URL = 'https://lolquiz-cms.herokuapp.com/questions?_sort=id&_limit={}&_start={}'
 HTTP_STATUS_ERROR_CODES = [408, 502, 503, 504]
+TOTAL_QUESTIONS = 100
+INIT_OFFSET = 0
 
 
 def get_questions(url=LOLQUIZ_CMS_URL):
@@ -13,7 +15,36 @@ def get_questions(url=LOLQUIZ_CMS_URL):
     retries = Retry(total=5, backoff_factor=1, status_forcelist=HTTP_STATUS_ERROR_CODES)
     s.mount('https://', HTTPAdapter(max_retries=retries))
 
-    response = s.get(url)
-    response_as_json = json.loads(response.content)
+    offset = INIT_OFFSET
+    all_questions = []
+    while True:
+        url = LOLQUIZ_CMS_URL.format(TOTAL_QUESTIONS, offset)
+        response = s.get(url)
+        if not has_content(response):
+            break
+        all_questions.append(json.loads(response.content))
+        offset += TOTAL_QUESTIONS
 
-    return response_as_json
+    return [set_question(question)
+            for outer_questions in all_questions
+            for question in outer_questions if valid_question(question)]
+
+
+def has_content(response):
+    return len(json.loads(response.content)) > 0
+
+
+def valid_question(question):
+    return 'id' in question \
+           and 'title' in question \
+           and 'game_type' in question and question['game_type'] is not None \
+           and 'game_modes' in question and len(question['game_modes']) > 0 \
+           and 'options' in question
+
+
+def set_question(question):
+    return {'id': question['id'],
+            'title': question['title'],
+            'game_type': question['game_type'],
+            'options': question['options'],
+            'game_modes': question['game_modes']}
